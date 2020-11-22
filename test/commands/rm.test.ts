7d78b96487b70aafc962aed00acc3681f25bddf4
RayhanHamada <hamadadev2016@gmail.com>
2020-11-22 18:23:08 +0700

    remove oclif.manifest

--- test/commands/rm.test.ts
+++ test/commands/rm.test.ts
@@ -1,10 +1,10 @@
-// import { expect, test } from '@oclif/test';
+  // import { expect, test } from '@oclif/test';
 
-// describe('rm', () => {
-//   test
-//     .stdout()
-//     .command(['rm'])
-//     .it('runs hello', ctx => {
-//       expect(ctx.stdout).to.contain('hello world');
-//     });
-// });
+  // describe('rm', () => {
+  //   test
+  //     .stdout()
+  //     .command(['rm'])
+  //     .it('runs hello', ctx => {
+  //       expect(ctx.stdout).to.contain('hello world');
+  //     });
+  // });
